36488dc0902f730dc3051ab3d710caaa1a35a6fd
Tomasz Wąchała <tomekschark@wp.pl>
2023-10-11 15:34:04 +0200

    dodawanie na początek i koniec

--- test.cpp
+++ test.cpp
@@ -3,13 +3,43 @@
 
 using namespace std;
 
+class test
+{
+private:
+    list<int> mylist;
+public:
+    test(){
+        mylist = {1, 2, 3, 4, 5};
+    };
+    ~test(){
+        mylist.clear();
+    };
+    void wypisz(){
+        for (int numbers : mylist) {
+            cout << numbers << " ";
+        }
+        cout << endl;
+    };
+    void dodajp(int number){
+        mylist.push_front(number);
+    };
+    void dodajt(int number){
+        mylist.push_back(number);
+    };
+};
+
 int main() {
-    list<int> mylist = {1, 2, 3, 4, 5};
-    
-    for (int numbers : mylist) {
-        cout << numbers << " ";
-    }
-    cout << endl;
+    test lista;
+
+    lista.wypisz();
+
+    cout << "Dodajemy 0 na poczatek" << endl;
+    lista.dodajp(0);
+    lista.wypisz();
+
+    cout << "Dodajemy 6 na koniec" << endl;
+    lista.dodajt(6);
+    lista.wypisz();
 
     return 0;
 }

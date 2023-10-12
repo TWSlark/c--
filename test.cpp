0b358d7f18aa4dc4e7b0fbb18a49a78a17c5eff3
Tomasz Wąchała <tomekschark@wp.pl>
2023-10-12 13:04:37 +0200

    usuwanie elementów z początku, z końca, z dowolnego miejsca

--- test.cpp
+++ test.cpp
@@ -64,7 +64,30 @@ public:
         nowy->prev = temp;
         temp->next = nowy;
     };
-    
+    void usun_pierwsze(){
+        node* temp = head;
+        head = head->next;
+        head->prev = NULL;
+        delete temp;
+    };
+    void usun_ostatnie(){
+        node* temp = head;
+        while(temp->next->next != NULL){
+            temp = temp->next;
+        }
+        delete temp->next;
+        temp->next = NULL;
+    };
+    void usun_na_miejscu(int index){
+        node* temp = head;
+        for(int i = 0; i < index-2; i++){
+            temp = temp->next;
+        }
+        node* temp2 = temp->next;
+        temp->next = temp->next->next;
+        temp->next->prev = temp;
+        delete temp2;
+    };
 };
 
 int main() {
@@ -78,12 +101,25 @@ int main() {
     lista.z_przodu(2);
     lista.wypisz();
 
-    cout << "Dodanie elementu 6 na koniec" << endl;
+    cout << "Dodanie elementu 6 i 7 na koniec" << endl;
     lista.z_tylu(6);
+    lista.z_tylu(7);
     lista.wypisz();
 
-    cout << "Dodanie elementu 2 na drugie miejsce" << endl;
+    cout << "Dodanie elementu 4 pod drugim elemencie" << endl;
     lista.na_miejscu(4, 2);
+    lista.wypisz();
+
+    cout << "Usuniecie pierwszego elementu" << endl;
+    lista.usun_pierwsze();
+    lista.wypisz();
+
+    cout << "Usuniecie ostatniego elementu" << endl;
+    lista.usun_ostatnie();
+    lista.wypisz();
+
+    cout << "Usuniecie elementu na trzecim miejscu" << endl;
+    lista.usun_na_miejscu(3);
     lista.wypisz();
 
     return 0;
